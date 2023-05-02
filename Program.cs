6083958b87c2e75229c8a5a1ced4e77edf86b544
kristofeeerr <123837931+kristofeeerr@users.noreply.github.com>
2023-05-03 00:41:21 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tryCount = 10;
+            string completionWord = "exit";
+            string userInput;
+            
+            for(int i = 0; i < tryCount; i++) 
+            {
+                Console.Write("Введите заврещающие слово:");
+                userInput = Console.ReadLine();
+
+                if(completionWord == userInput) 
+                {
+                    Console.WriteLine("До свидания!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("У вас осталось - " + (tryCount - i - 1 ) + " попыток");
+                }
+            }
+                
+        }
+    }
+}
